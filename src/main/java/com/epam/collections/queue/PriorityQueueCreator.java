3889986cf636e1b563dfeff73616521e0096d84a
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-08-25 10:27:17 +0600

    Update PriorityQueueCreator.java

--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -2,6 +2,7 @@ package com.epam.collections.queue;
 
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.*;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
