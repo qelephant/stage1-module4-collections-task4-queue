dfe5fba45efd896b8e24b00fe2860c648c0f4b95
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-08-25 10:25:24 +0600

    Update DishOrderDeterminer.java

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -4,5 +4,17 @@ import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> integerList = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numberOfDishes; i++) {
+            queue.add(i);
+        }
+        List<Integer> tempList = new ArrayList<>(queue);
+
+        for (Integer element : tempList) {
+            if(element % everyDishNumberToEat == 0)
+            integerList.add = element;
+        }
+        return integerList;
     }
 }
